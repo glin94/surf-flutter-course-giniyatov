146cd483c9445abf1e3f000224c84aeb8d12b5b3
Linar Giniyatov <narikus94@gmail.com>
2021-03-14 23:42:31 +0300

    sliverpersistentheader appbar added

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -21,87 +21,108 @@ class SightListScreen extends StatelessWidget {
     return Scaffold(
       floatingActionButton: const AddSightButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) =>
-            <Widget>[
-          innerBoxIsScrolled
-              ? const SliverAppBar(
-                  automaticallyImplyLeading: false,
-                  title: Text(sightListScreenTitle),
-                  pinned: true,
-                  elevation: 0,
-                )
-              : const _LargeAppBar(),
-        ],
-        body: StreamBuilder<List<Sight>>(
-            initialData: mocks,
-            stream: sightInteractor.sightListStream,
-            builder: (context, snapshot) {
-              final sightList = snapshot.data;
-              return CustomScrollView(
-                slivers: <Widget>[
-                  SliverAppBar(
-                    automaticallyImplyLeading: false,
-                    elevation: 0,
-                    primary: false,
-                    title: GestureDetector(
+      body: StreamBuilder<List<Sight>>(
+          initialData: mocks,
+          stream: sightInteractor.sightListStream,
+          builder: (context, snapshot) {
+            final sightList = snapshot.data;
+            return CustomScrollView(
+              physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics(),
+              ),
+              slivers: <Widget>[
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
+                  ),
+                  sliver: SliverPersistentHeader(
+                    pinned: true,
+                    delegate: _AppBarPersistentHeaderDelegate(
+                      130,
+                      MediaQuery.of(context).size.height / kToolbarHeight * 2,
+                    ),
+                  ),
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 18,
+                  ),
+                  sliver: SliverToBoxAdapter(
+                    child: GestureDetector(
                       onTap: () =>
                           Navigator.of(context).push(CupertinoPageRoute(
                         builder: (context) => SightSearchScreen(),
                       )),
-                      child: const SearchBar(
+                      child: SearchBar(
                         enable: false,
                       ),
                     ),
                   ),
-                  CupertinoSliverRefreshControl(
-                    onRefresh: () async {
-                      await Future.delayed(Duration(seconds: 2));
-                    },
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 16,
                   ),
-                  SliverPadding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 24,
-                    ),
-                    sliver: SliverList(
-                      delegate: SliverChildBuilderDelegate(
-                        (BuildContext context, int index) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 12),
-                            child: SightCard(sight: sightList[index]),
-                          );
-                        },
-                        childCount: sightList.length,
-                      ),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 24),
+                          child: SightCard(sight: sightList[index]),
+                        );
+                      },
+                      childCount: sightList.length,
                     ),
                   ),
-                ],
-              );
-            }),
-      ),
+                ),
+              ],
+            );
+          }),
     );
   }
 }
 
-class _LargeAppBar extends StatelessWidget {
-  const _LargeAppBar({
-    Key key,
-  }) : super(key: key);
+///Делегат для AppBar
+class _AppBarPersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
+  _AppBarPersistentHeaderDelegate(
+    this.expandedHeight,
+    this.titlePaddingTop,
+  );
+
+  final double expandedHeight;
+
+  final double titlePaddingTop;
 
   @override
-  Widget build(BuildContext context) {
-    return SliverAppBar(
-      automaticallyImplyLeading: false,
-      elevation: 0,
-      centerTitle: false,
-      toolbarHeight: 120,
-      title: Text(
-        sightListScreenTitle.replaceFirst(" ", "\n"),
-        maxLines: 2,
-        textAlign: TextAlign.left,
-        style: Theme.of(context).textTheme.headline4,
-      ),
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return AppBar(
+      toolbarHeight: expandedHeight,
+      titleSpacing: 0,
+      centerTitle: !(expandedHeight - minExtent > shrinkOffset),
+      title: expandedHeight - minExtent > shrinkOffset
+          ? Text(
+              sightListScreenTitle.replaceFirst(" ", "\n"),
+              style: Theme.of(context).textTheme.headline4,
+            )
+          : Text(sightListScreenTitle),
     );
+  }
+
+  @override
+  double get maxExtent => expandedHeight;
+
+  @override
+  double get minExtent => expandedHeight - titlePaddingTop;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
   }
 }
